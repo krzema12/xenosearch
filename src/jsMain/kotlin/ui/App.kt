604fead12c7f24e38b643a770b31dd5e71ce6cd3
Piotr Krzeminski <git@krzeminski.it>
2023-11-15 21:23:31 +0100

    Tweak UI

--- src/jsMain/kotlin/ui/App.kt
+++ src/jsMain/kotlin/ui/App.kt
@@ -6,7 +6,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import logic.analyzeIngredients
+import org.jetbrains.compose.web.attributes.cols
+import org.jetbrains.compose.web.attributes.rows
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.color
 import org.jetbrains.compose.web.dom.Br
+import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Li
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.dom.TextArea
@@ -16,10 +21,16 @@ import org.jetbrains.compose.web.dom.Ul
 fun App() {
     var ingredients by remember { mutableStateOf("") }
 
+    Div(attrs = { style { color(Color.red) } }) {
+        Text("Aplikacja niegotowa do publikacji, w trakcie budowy")
+    }
+
     Text("Wklej skład produktu w poniższe pole:")
     Br {}
 
     TextArea(value = ingredients) {
+        rows(20)
+        cols(80)
         onInput {
             println("Input")
             ingredients = it.value
@@ -30,7 +41,7 @@ fun App() {
     Ul {
         analyzeIngredients(ingredients).forEach { (knownXenoestrogen, isPresent) ->
             Li {
-                Text("$knownXenoestrogen: $isPresent")
+                Text("$knownXenoestrogen: ${if (isPresent) "⚠\uFE0F" else "⚪" }")
             }
         }
     }
