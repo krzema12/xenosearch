745151db94290691fcf3e097770af86535c58f01
Piotr Krzeminski <git@krzeminski.it>
2023-12-04 07:39:06 +0100

    Improve snapshot testing infra

--- src/jvmTest/kotlin/RealProductsTest.kt
+++ src/jvmTest/kotlin/RealProductsTest.kt
@@ -1,29 +1,19 @@
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import logic.analyzeIngredients
 import kotlin.io.path.Path
-import kotlin.io.path.readText
-import kotlin.io.path.writeText
 
 class RealProductsTest : FunSpec({
-    test("Deoproce SPF50") {
-        val currentlyStored = Path("src/jvmTest/resources/products/Deoproce SPF50-snapshot.json").readText()
-        val foundXenoestrogens = analyzeIngredients(ingredients = getProductIngredients("Deoproce SPF50"))
-        val sortedKeys = foundXenoestrogens.toSortedMap()
-        val serialized = json.encodeToString<Map<String, Boolean>>(sortedKeys)
-
-        currentlyStored shouldBe serialized
-
-        Path("src/jvmTest/resources/products/Deoproce SPF50-snapshot.json").writeText(serialized)
-    }
+    Path("src/jvmTest/resources/products").toFile()
+        .list { _, name -> name.endsWith(".txt") }
+        ?.map { it.removeSuffix(".txt") }
+        ?.forEach { testFileName ->
+            test(testFileName) {
+                val foundXenoestrogens = analyzeIngredients(ingredients = getProductIngredients(this.testCase.name.testName))
+                foundXenoestrogens.shouldMatchSnapshot(this)
+            }
+        } ?: error("No test files found!")
 })
 
 private fun getProductIngredients(name: String): String =
     RealProductsTest::class.java.getResource("products/$name.txt")?.readText()
         ?: error("Could not read the resource!")
-
-private val json = Json {
-    prettyPrint = true
-}
---
+++ src/jvmTest/kotlin/SnapshotTesting.kt
@@ -0,0 +1,24 @@
+import io.kotest.core.test.TestScope
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+
+internal fun Map<String, Boolean>.shouldMatchSnapshot(testScope: TestScope) {
+    val sortedKeys = this.toSortedMap()
+    val serialized = json.encodeToString<Map<String, Boolean>>(sortedKeys)
+    val pathToSnapshot = Path("src/jvmTest/resources/products/${testScope.testCase.name.testName}-snapshot.json")
+
+    if (System.getenv("UPDATE_SNAPSHOTS") == null) {
+        val currentlyStored = pathToSnapshot.readText()
+        currentlyStored shouldBe serialized
+    } else {
+        pathToSnapshot.writeText(serialized)
+    }
+}
+
+private val json = Json {
+    prettyPrint = true
+}
