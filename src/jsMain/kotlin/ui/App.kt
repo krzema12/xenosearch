f9601db2c4afcc572cde012603a935e01b35bca4
Piotr Krzeminski <git@krzeminski.it>
2023-12-04 15:15:18 +0100

    Clean up the UI layer

--- src/jsMain/kotlin/ui/App.kt
+++ src/jsMain/kotlin/ui/App.kt
@@ -9,8 +9,16 @@ import logic.analyzeIngredients
 import org.jetbrains.compose.web.attributes.cols
 import org.jetbrains.compose.web.attributes.rows
 import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.DisplayStyle
+import org.jetbrains.compose.web.css.FlexDirection
+import org.jetbrains.compose.web.css.boxSizing
 import org.jetbrains.compose.web.css.color
-import org.jetbrains.compose.web.dom.Br
+import org.jetbrains.compose.web.css.display
+import org.jetbrains.compose.web.css.flexDirection
+import org.jetbrains.compose.web.css.height
+import org.jetbrains.compose.web.css.margin
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Li
 import org.jetbrains.compose.web.dom.Text
@@ -21,23 +29,35 @@ import org.jetbrains.compose.web.dom.Ul
 fun App() {
     var ingredients by remember { mutableStateOf("") }
 
+    Column {
+        Header()
+        IngredientsInput(value = ingredients, onChange = { ingredients = it })
+        Results(ingredients)
+    }
+}
+
+@Composable
+private fun Header() {
     Div(attrs = { style { color(Color.red) } }) {
         Text("Aplikacja niegotowa do publikacji, w trakcie budowy")
     }
 
     Text("Wklej skład produktu w poniższe pole:")
-    Br {}
+}
 
-    TextArea(value = ingredients) {
+@Composable
+fun IngredientsInput(value: String, onChange: (String) -> Unit) {
+    TextArea(value = value) {
         rows(20)
         cols(80)
         onInput {
-            println("Input")
-            ingredients = it.value
+            onChange(it.value)
         }
     }
-    Br {}
+}
 
+@Composable
+private fun Results(ingredients: String) {
     Ul {
         analyzeIngredients(ingredients)
             .entries
@@ -48,5 +68,20 @@ fun App() {
                     Text("$knownXenoestrogen: $resultMarker")
                 }
             }
+    }
+}
+
+@Composable
+private fun Column(content: @Composable () -> Unit) {
+    Div({
+        style {
+            display(DisplayStyle.Flex)
+            flexDirection(FlexDirection.Column)
+            height(100.percent)
+            margin(0.px)
+            boxSizing("border-box")
+        }
+    }) {
+        content()
     }
 }
