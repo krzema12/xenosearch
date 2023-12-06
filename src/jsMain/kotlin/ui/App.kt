b4da6fefe2095571d2a1aa475f4c2ffee6e4683d
Piotr Krzeminski <git@krzeminski.it>
2023-12-06 21:39:54 +0100

    Use dialog to present warning

--- src/jsMain/kotlin/ui/App.kt
+++ src/jsMain/kotlin/ui/App.kt
@@ -5,27 +5,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import dev.petuska.kmdc.banner.Actions
-import dev.petuska.kmdc.banner.Content
-import dev.petuska.kmdc.banner.Graphic
-import dev.petuska.kmdc.banner.Icon
-import dev.petuska.kmdc.banner.MDCBanner
-import dev.petuska.kmdc.banner.PrimaryAction
 import dev.petuska.kmdc.data.table.Body
 import dev.petuska.kmdc.data.table.Cell
 import dev.petuska.kmdc.data.table.Container
 import dev.petuska.kmdc.data.table.MDCDataTable
 import dev.petuska.kmdc.data.table.MDCDataTableHeader
 import dev.petuska.kmdc.data.table.Row
+import dev.petuska.kmdc.dialog.Action
+import dev.petuska.kmdc.dialog.Actions
+import dev.petuska.kmdc.dialog.CloseButton
+import dev.petuska.kmdc.dialog.Content
+import dev.petuska.kmdc.dialog.Header
+import dev.petuska.kmdc.dialog.MDCDialog
+import dev.petuska.kmdc.dialog.Title
+import dev.petuska.kmdc.dialog.onClosed
 import dev.petuska.kmdc.textfield.MDCTextArea
 import dev.petuska.kmdc.typography.MDCBody1
 import dev.petuska.kmdcx.icons.MDCIcon
 import dev.petuska.kmdcx.icons.mdcIcon
 import logic.analyzeIngredients
-import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.FlexDirection
-import org.jetbrains.compose.web.css.backgroundColor
 import org.jetbrains.compose.web.css.boxSizing
 import org.jetbrains.compose.web.css.display
 import org.jetbrains.compose.web.css.flexDirection
@@ -34,12 +34,32 @@ import org.jetbrains.compose.web.css.margin
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
-import dev.petuska.kmdc.banner.Text as BannerText
 import org.jetbrains.compose.web.dom.Text as ComposeText
 
 @Composable
 fun App() {
     var ingredients by remember { mutableStateOf("") }
+    var warningDialogOpen by remember { mutableStateOf(true) }
+
+    MDCDialog(
+        open = warningDialogOpen,
+        fullscreen = true,
+        stacked = false,
+        attrs = {
+            onClosed { warningDialogOpen = false }
+        },
+    ) {
+        Header {
+            Title("Ostrzeżenie")
+            CloseButton(MDCIcon.Close.type, attrs = { mdcIcon() })
+        }
+        Content {
+            ComposeText("Aplikacja niegotowa do publikacji, w trakcie budowy!")
+        }
+        Actions {
+            Action(action = "ok", text = "OK")
+        }
+    }
 
     Column {
         Header()
@@ -50,18 +70,6 @@ fun App() {
 
 @Composable
 private fun Header() {
-    MDCBanner(open = true, attrs = { style { backgroundColor(Color.pink) } }) {
-        Content {
-            Graphic {
-                Icon(attrs = { mdcIcon() }) { ComposeText(MDCIcon.Warning.type) }
-            }
-            BannerText("Aplikacja niegotowa do publikacji, w trakcie budowy")
-        }
-        Actions {
-            PrimaryAction("OK")
-        }
-    }
-
     MDCBody1("Wklej skład produktu w poniższe pole:")
 }
 
